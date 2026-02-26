1b17aa45a3819f2f353cefda0436216293305650
ceichhorst <ceichhorst1@madisoncollege.edu>
2026-02-25 23:04:25 -0600

    Adjusted index jsp to show database data

--- src/main/java/com.ceichhorst.reservation/controller/HomeServlet.java
+++ src/main/java/com.ceichhorst.reservation/controller/HomeServlet.java
@@ -1,15 +1,15 @@
 package com.ceichhorst.reservation.controller;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 @WebServlet("/")
 public class HomeServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServlet request, HttpServletResponse responsed)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
              throws ServletException, IOException {
 
         request.setAttribute("message", "Servlet is working!");

8235b63b466ac67a5c4028a496126973dae8d85f
ceichhorst <ceichhorst1@madisoncollege.edu>
2026-03-29 21:08:03 -0500

    Added ApplicationConfig

---
+++ src/main/java/com.ceichhorst.reservation/controller/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.ceichhorst.reservation.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+}
